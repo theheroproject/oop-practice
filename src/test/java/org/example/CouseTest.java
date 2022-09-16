7dfe226623e37433b688a834e9bcdaef0062f809
theheroproject <58100691+theheroproject@users.noreply.github.com>
2022-09-16 21:33:22 +0900

    commit

---
+++ src/test/java/org/example/CouseTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CouseTest {
+
+    @DisplayName("test")
+    @Test
+    void createTest(){
+        assertThatCode(())
+
+    }
+}
---
+++ src/test/java/org/example/GradeCalculatorTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class GradeCalculatorTest {
+    // 単位計算　ドメイン：受講した科目、単位計算
+    //　受講した科目：
+
+    @Test
+    void calculaterGradeTest(){
+
+    }
+
+}
